ec343be62f2d4b3d2a9c276304a569cef97b198c
Colder777 <91277137+ShangChi-IsNice@users.noreply.github.com>
2021-11-27 13:55:40 -0500

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,31 @@
+document.getElementById('back-to-top').addEventListener("click", function(){
+    window.scrollTo(0 , 0);
+});
+
+document.getElementById('nav-sec-1').addEventListener("click", function(){
+    document.getElementById('section-1').scrollIntoView();
+});
+
+document.getElementById('nav-sec-2').addEventListener("click", function(){
+    document.getElementById('section-2').scrollIntoView();
+});
+
+document.getElementById('nav-sec-3').addEventListener("click", function(){
+    document.getElementById('section-3').scrollIntoView();
+});
+
+document.getElementById('nav-sec-4').addEventListener("click", function(){
+    document.getElementById('section-4').scrollIntoView();
+});
+
+document.getElementById('nav-sec-5').addEventListener("click", function(){
+    document.getElementById('section-5').scrollIntoView();
+});
+
+document.getElementById('nav-sec-6').addEventListener("click", function(){
+    document.getElementById('section-6').scrollIntoView();
+});
+
+document.getElementById('nav-sec-7').addEventListener("click", function(){
+    document.getElementById('section-7').scrollIntoView();
+});
